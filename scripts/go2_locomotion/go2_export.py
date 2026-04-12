40dec41b919680b8155d5d2e258276a7a732598f
kindsenior <k-kojima@jsk.imi.i.u-tokyo.ac.jp>
2026-04-12 20:50:20 +0900

    add exporter of policy

---
+++ scripts/go2_locomotion/go2_export.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from go2_env import Go2Env
+from genesis_tools.exporter import dump_policy
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--log_dir", type=str, required=True)
+    parser.add_argument("--ckpt", type=int, default=100)
+    args = parser.parse_args()
+
+    dump_policy(
+        env_class=Go2Env,
+        log_dir=args.log_dir,
+        ckpt=args.ckpt,
+        env_kwargs={"show_viewer": True},
+    )
+
+
+if __name__ == "__main__":
+    main()
